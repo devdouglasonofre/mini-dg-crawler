4e3854c3b729a006006da2a0ac1fb2c4fe047057
Douglas Onofre <douglas12psp@gmail.com>
2024-02-18 15:15:11 +0000

    Groundwork for i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
+// @ts-nocheck
+
 	import '../app.css';
+	import en from '$locales/en';
+	
+	
+	import { addMessages, init } from 'svelte-intl-precompile';
+	import { t } from 'svelte-intl-precompile'
+	addMessages('en', en);
+	init({
+		initialLocale: 'en',
+		fallbackLocale: 'en'
+	});
+
+	
 </script>
 
+<h1>{$t('BATTLE.ATTACK')}</h1>
 <slot />
